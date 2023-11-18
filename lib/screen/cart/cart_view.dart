200f79980df0ffe75bf599b01a0241c7e90da145
bta <tuananhvip1099@gmail.com>
2023-11-18 13:49:07 +0700

    TA build UI

---
+++ lib/screen/cart/cart_view.dart
@@ -0,0 +1,124 @@
+import 'package:do_an_di_dong/screen/order_payment/components/cart_item.dart';
+import 'package:do_an_di_dong/screen/order_payment/order_payment_view.dart';
+import 'package:flutter/material.dart';
+
+class CartView extends StatefulWidget {
+  const CartView({super.key});
+
+  @override
+  State<CartView> createState() => _CartViewState();
+}
+
+class _CartViewState extends State<CartView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.5,
+        title: const Text('Giỏ hàng của (Username)'),
+        centerTitle: true,
+        titleTextStyle: const TextStyle(
+          fontSize: 15,
+          color: Colors.black,
+          fontWeight: FontWeight.w400,
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.close,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 15),
+            color: Colors.grey.withOpacity(0.5),
+            child: Row(
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: const Icon(Icons.check_box_outline_blank),
+                ),
+                const SizedBox(width: 5),
+                RichText(
+                  text: const TextSpan(
+                    text: 'Chọn tất cả ',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: '(2)',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Column(
+            children: [
+              CartItemWiget(),
+              CartItemWiget(),
+              CartItemWiget(),
+            ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+        height: 70,
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Colors.black,
+            ),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Tạm Tính',
+                  style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '30,000đ',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff79AC78),
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              style: ButtonStyle(backgroundColor: MaterialStateColor.resolveWith((states) => const Color(0xff79AC78))),
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) {
+                    return const OrderPaymentView();
+                  },
+                ));
+              },
+              child: const Text('Đặt mua (3)'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen/order_detail/order_detail.dart
@@ -0,0 +1,327 @@
+import 'package:flutter/material.dart';
+
+class OrderDetailView extends StatefulWidget {
+  const OrderDetailView({super.key});
+
+  @override
+  State<OrderDetailView> createState() => _OrderDetailViewState();
+}
+
+class _OrderDetailViewState extends State<OrderDetailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.5,
+        title: const Text('Chi tiết đơn hàng'),
+        centerTitle: true,
+        titleTextStyle: const TextStyle(
+          fontSize: 15,
+          color: Colors.black,
+          fontWeight: FontWeight.w400,
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.close,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Nguyễn Văn A - 25484884',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Divider(
+                  color: Colors.black,
+                  height: 5,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'Nhận hàng tại',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 5),
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.grey.withOpacity(0.3),
+                  ),
+                  child: const Column(
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.home_work_outlined),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Text(
+                              'Số nhà 123,đuờng 123123',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.phone_enabled_outlined),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            '04848848484',
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 5,
+            color: Colors.grey.withOpacity(0.5),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Mã đơn hàng',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+                Text(
+                  '6151515151511151551',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 5),
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 15),
+            color: const Color(0xffBDF1E8),
+            child: Row(
+              children: [
+                const Icon(Icons.calendar_month_outlined),
+                const SizedBox(width: 5),
+                RichText(
+                  text: const TextSpan(
+                    text: 'Thời gian nhận: ',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'Từ 16h,Thứ 6 (13/11)',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            child: Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 5),
+                  width: 75,
+                  height: 75,
+                  child: Image.asset('assets/images/Rau/cai_thia.jpg'),
+                ),
+                const Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            //hiển thị productNo hoặc name thì tùy anh nha
+                            'Tên SP + quy cách',
+                            maxLines: 1,
+                            style: TextStyle(
+                              fontSize: 12,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            '9,900 đ',
+                            maxLines: 1,
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  ),
+                ),
+                const Text(
+                  'x1',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(
+            height: 3,
+            color: Colors.black,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Thời gian thanh toán',
+                      style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold, color: Colors.grey.withOpacity(0.8)),
+                    ),
+                    const Text(
+                      '16:30 - 2/11/2023',
+                      style: TextStyle(
+                        fontSize: 11,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Phương thức thanh toán',
+                      style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold, color: Colors.grey.withOpacity(0.8)),
+                    ),
+                    const Text(
+                      'Thanh toán khi nhận hàng',
+                      style: TextStyle(
+                        fontSize: 11,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Số tiên thanh toán',
+                      style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold, color: Colors.grey.withOpacity(0.8)),
+                    ),
+                    const Text(
+                      '900,000đ',
+                      style: TextStyle(
+                        fontSize: 11,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const Divider(
+            height: 3,
+            color: Colors.black,
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+        height: 60,
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Colors.black,
+            ),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                style: ButtonStyle(backgroundColor: MaterialStateColor.resolveWith((states) => const Color(0xff79AC78))),
+                onPressed: () {},
+                child: const Text('Mua lại'),
+              ),
+            ),
+            const SizedBox(
+              width: 15,
+            ),
+            Expanded(
+              child: ElevatedButton(
+                style: ButtonStyle(backgroundColor: MaterialStateColor.resolveWith((states) => const Color(0xff79AC78))),
+                onPressed: () {},
+                child: const Text('Phản hồi'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
